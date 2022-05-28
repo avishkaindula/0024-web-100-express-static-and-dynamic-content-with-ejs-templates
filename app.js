4f116bd10da5ef169ef8c0017f702d834c100fb2
Avishka Indula <avishkainduladev@gmail.com>
2022-05-28 11:03:25 +0530

    serving HTML files with Node and Express

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>");
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.send(htmlFilePath);
+});
+
+app.get("/restaurants", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  res.sendFile(htmlFilePath);
+  // This is how we serve a file (such as a HTML file) as the response.
+});
+
+app.get("/recommend", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  res.send(htmlFilePath);
+});
+
+app.get("/confirm", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  res.send(htmlFilePath);
+});
+
+app.get("/about", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
+  res.send(htmlFilePath);
 });
 
 app.listen(3000);
